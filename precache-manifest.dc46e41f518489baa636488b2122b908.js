aa324732e35704c2ef2ea7e194d986b500baafad
Carolina Martinez <carolina@Carolinas-iMac.local>
2018-11-01 16:53:54 +0000

    Updates

---
+++ precache-manifest.dc46e41f518489baa636488b2122b908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4653d22f4ffe8ca779f7",
+    "url": "/airport-search-app/static/css/main.eaa0598e.chunk.css"
+  },
+  {
+    "revision": "4653d22f4ffe8ca779f7",
+    "url": "/airport-search-app/static/js/main.4653d22f.chunk.js"
+  },
+  {
+    "revision": "471222d069da19fb9ceb",
+    "url": "/airport-search-app/static/js/1.471222d0.chunk.js"
+  },
+  {
+    "revision": "a711c05116b7b990146c",
+    "url": "/airport-search-app/static/js/runtime~main.a711c051.js"
+  },
+  {
+    "revision": "c47b4035e6724193aaca4f5f400534d4",
+    "url": "/airport-search-app/index.html"
+  }
+];
